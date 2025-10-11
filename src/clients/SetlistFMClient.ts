2aa01c1c4b68fedea75fb93d7602142efe76b833
Tyler Agnew <tyler.agnew@gmail.com>
2025-10-11 11:15:04 -0400

    fixing setlistfm call

--- src/clients/SetlistFMClient.ts
+++ src/clients/SetlistFMClient.ts
@@ -15,14 +15,16 @@ const headers = {
 
 @Injectable()
 export class SetlistFMClient {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(private readonly httpService: HttpService) { }
   async getSetlistsByArtistName(artistMBID: string) {
     try {
       // Setlist.FM automatically shows the most recent shows first.
       const response = await this.httpService
-        .get(SETLIST_FM_BASE_URL + `artist/${artistMBID}/setlists}`, {
-          headers,
-        })
+        .get(
+          SETLIST_FM_BASE_URL +
+          `artist/${artistMBID}/setlists?`,
+          { headers },
+        )
         .toPromise();
       return response.data;
     } catch (error) {
